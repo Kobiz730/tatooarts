190f1029ff0bb01b51eaa53414e825171b668f4a
nocodeytb <nocodeytb@gmail.com>
2019-01-16 09:28:17 -0500

    remove acces token

--- app/bot/bot.rb
+++ app/bot/bot.rb
@@ -1,8 +1,6 @@
 require 'facebook/messenger'
 include Facebook::Messenger
 
-Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCES_TOKEN"])
-
 Bot.on :message do |message|
   message.reply(text: 'Hello, human!')
 end
